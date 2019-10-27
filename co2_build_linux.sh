be3f87ac838b6fb274a5e1f99fbccb30446e3e26
Nicolas <nicolas@uucidl.com>
2019-10-27 22:07:13 +0100

    Add missing udev lib.

--- co2_build_linux.sh
+++ co2_build_linux.sh
@@ -5,7 +5,7 @@ CC=${CC:-cc}
 (O="${HERE}"/co2
  "${CC}" "${HERE}"/src/co2_unit.c -g -o "${O}" -I"${HERE}"/deps/hidapi/hidapi \
     "${HERE}"/deps/hidapi/linux/hid.c \
-    -DLINUX_FREEBSD -DHIDAPI=hidraw \
+    -DLINUX_FREEBSD -DHIDAPI=hidraw -ludev \
     && printf "PROGRAM\t%s\n" "${O}") || exit 1
 
 exit 0
